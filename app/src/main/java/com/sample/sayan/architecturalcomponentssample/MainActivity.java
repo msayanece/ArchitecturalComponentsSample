816afe90cad081225af9647669d5e989b38bfcef
sayan <anshu.p@encoders.co.in>
2019-02-01 16:47:51 +0530

    import clean up

--- app/src/main/java/com/sample/sayan/architecturalcomponentssample/MainActivity.java
+++ app/src/main/java/com/sample/sayan/architecturalcomponentssample/MainActivity.java
@@ -1,25 +1,18 @@
 package com.sample.sayan.architecturalcomponentssample;
 
 import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.sample.sayan.architecturalcomponentssample.databinding.ActivityMainBinding;
 
--- app/src/main/java/com/sample/sayan/architecturalcomponentssample/WordAdapter.java
+++ app/src/main/java/com/sample/sayan/architecturalcomponentssample/WordAdapter.java
@@ -1,20 +1,14 @@
 package com.sample.sayan.architecturalcomponentssample;
 
-import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.sample.sayan.architecturalcomponentssample.databinding.WordChildBinding;
 
 import java.util.List;
-
-import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class WordAdapter extends RecyclerView.Adapter<WordAdapter.WordViewHolder>  {
     private List<Word> words;
